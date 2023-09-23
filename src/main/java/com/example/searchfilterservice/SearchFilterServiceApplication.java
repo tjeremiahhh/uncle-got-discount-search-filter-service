9445bbcab5335495da6917405c6b65d0ffcef751
Jeremiah Tan <axel.max97@gmail.com>
2023-09-23 15:03:14 +0800

    Add to remote Github

---
+++ src/main/java/com/example/searchfilterservice/SearchFilterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.searchfilterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchFilterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchFilterServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/searchfilterservice/SearchFilterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.searchfilterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchFilterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
