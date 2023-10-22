ec5fb161f9ac6d3e2eba13966be3e6e1350577ee
xaviertanwc <66908992+xaviertanwc@users.noreply.github.com>
2023-10-23 00:31:55 +0800

    ✨ Search bar api

--- src/main/java/com/example/searchfilter/common/repository/SimpleJdbcRepositoryImpl.java
+++ src/main/java/com/example/searchfilter/common/repository/SimpleJdbcRepositoryImpl.java
@@ -15,7 +15,9 @@ import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.CollectionUtils;
+
 import com.example.searchfilter.common.utility.SqlUtil;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Repository
--- src/test/java/com/example/searchfilter/SearchFilterServiceApplicationTests.java
+++ src/test/java/com/example/searchfilter/SearchFilterServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.searchfilterservice;
+package com.example.searchfilter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
